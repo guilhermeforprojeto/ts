db801d1ff81f1726cfd0652c1127e02b104ec51f
guilhermeforprojeto <guylhermesantos@hotmail.com>
2021-05-30 22:22:40 -0300

    Brincando com FOR array:string[] IF, fiz um loop

---
+++ brincando.ts
@@ -0,0 +1,16 @@
+// ==> Exemplo 06 - Exemplo de Array com laço de iteração
+
+let linguagensArray:string[] = new Array('','JavaScript', 'Python', 'PHP', 'C#','');
+
+function funcaoLinguagens(linguagens:string[]) {
+          for (let i = -1; i < linguagensArray.length; i++) {
+
+            console.log(linguagensArray[i]);
+            if (i >= 4) {
+              i = 0
+            }
+
+          }
+        
+}
+funcaoLinguagens(linguagensArray);
---
+++ build/brincando.js
@@ -0,0 +1,12 @@
+"use strict";
+// ==> Exemplo 06 - Exemplo de Array com laço de iteração
+let linguagensArray = new Array('', 'JavaScript', 'Python', 'PHP', 'C#', '');
+function funcaoLinguagens(linguagens) {
+    for (let i = -1; i < linguagensArray.length; i++) {
+        console.log(linguagensArray[i]);
+        if (i >= 4) {
+            i = 0;
+        }
+    }
+}
+funcaoLinguagens(linguagensArray);
