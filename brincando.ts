4907ddb06991a99b6012c7f6277be0c1aa7e32e8
guilhermeforprojeto <guylhermesantos@hotmail.com>
2021-06-01 02:40:41 -0300

    Melhorando codigo de brincando.ts

--- brincando.ts
+++ brincando.ts
@@ -1,16 +1,15 @@
 // ==> Exemplo 06 - Exemplo de Array com laço de iteração
 
-let linguagensArray:string[] = new Array('','JavaScript', 'Python', 'PHP', 'C#','');
+let linguagensArray:string[] = new Array('JavaScript', 'Python', 'PHP', 'C#');
 
 function funcaoLinguagens(linguagens:string[]) {
-          for (let i = -1; i < linguagensArray.length; i++) {
+          
+          for (let i = 0; i < linguagensArray.length; i++) {
 
             console.log(linguagensArray[i]);
-            if (i >= 4) {
-              i = 0
-            }
-
+            if (i = 4) {
+               i = 0
+               }
           }
-        
 }
-funcaoLinguagens(linguagensArray);
+          funcaoLinguagens(linguagensArray);
